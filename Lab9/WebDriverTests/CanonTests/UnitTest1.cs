b055bca51b1e6e1c6fcbd9266848c69aef32c6dd
Just1ks <artphresl@gmail.com>
2022-11-13 23:36:24 +0300

    update lab9

--- Lab9/WebDriverTests/CanonTests/UnitTest1.cs
+++ Lab9/WebDriverTests/CanonTests/UnitTest1.cs
@@ -8,12 +8,23 @@ namespace FunctionalTests
     public class Tests
     {
         [Test]
-        public void FindingWIFIHomeColorPrinter()
+        public void FindingColorPrinterEPSON()
         {
             IWebDriver driver = new ChromeDriver();
 
-            driver.Url = "https://www.canon.ru/";
+            driver.Url = "https://www.21vek.by/";
 
+            driver.FindElement(By.Id("catalogSearch")).SendKeys("Принтеры");
+            driver.FindElement(By.Id("catalogSearch")).SendKeys(Keys.Enter);
+
+            driver.FindElement(By.XPath("//*[@id='j-filter__form']/div[2]/dl/div/dd[4]/label")).Click();
+
+            driver.FindElement(By.XPath("//*[@id='j-filter__form']/div[3]/dl[4]")).Click();
+            driver.FindElement(By.XPath("//*[@id='j-filter__form']/div[3]/dl[4]/div/dd[1]/label")).Click();
+
+            driver.FindElement(By.Id("j-filter__btn")).Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
         }
     }
 }
